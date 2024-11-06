2d13d26fffcf3a43d9aa44591f3d1d13a52e836e
shariar-sheikh <shariarprog@gmail.com>
2024-11-06 20:29:15 +0600

    all file added

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      "fontFamily":{
+        'open':["Playwrite GB S, serif"]
+      },
+      colors:{
+        'shariar':'#ffafcc'
+      }
+    },
+  },
+  plugins: [],
+}
+
